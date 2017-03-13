7064dfae535eb5e3f66ad54222bb5ffadf6852b5
John Doe <kristen.fanning@northwestern.edu>
2017-03-13 17:12:04 -0500

    update questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,60 +6,70 @@ var questionsObj = [
 		choices: ["Caligula", "Nero", "Commodus", "Constantine"],
 		answer: "Caligula",
 		funFact: "Caligula means 'little boots' in Latin. Unsurprisingly, he didn't refer to himself that way.",
+		img: "assets/images/caligula.jpg",
 	},
 	{
-		question:"What substance did the Romans use on injuries that we now know is an antibacterial?",
-		choices:["Olive Oil","Lemon", "Honey", "Fish Oil"],
-		answer:"Honey",
-		funFact: "Romans saved the olive oil they used for bathing because they believed it had healing properties.",
+		question:"Who missed a battle in the east because he stopped at a brothel?",
+		choices:["Macrinus","Caracalla", "Lucius Verus", "Geta"],
+		answer:"Lucius Verus",
+		funFact: "He ruled jointly with Marcus Aurelius. Or, more accurately, Marcus did the work while Lucius partied, often at the pub he built in the palace.",
+		img: "assets/images/lucius.jpg",
 	},
 	{
 		question:"Which Roman Emperor celebrated buying the office by eating his predecessor's last meal?",
 		choices:["Didius Julianus", "Septimius Severus", "Antoninus Pius", "Agrippa"],
 		answer:"Didius Julianus",
-		funFact:"He paid 25,000 sesterces to every soldier to be declared Emperor. They took his money, then killed him 66 days later."
+		funFact:"He paid 25,000 sesterces to every soldier to be declared Emperor. They took his money, then killed him 66 days later.",
+		img: "assets/images/didius.jpg",
 	},
 	{
 		question:"Which of Nero's wives was actually a man whom Nero made dress as a woman?",
 		choices:["Poppaea Sabina", "Sporus", "Claudia Octavia", "Statila Messalina"],
 		answer:"Sporus",
 		funFact: "Poor Sporus looked so like the late Poppaea Sabina that even after Nero died, he was forced to be mistress to Poppaea's ex-husband.",
+		img: "assets/images/nero.JPG",
 	},
 	{
 		question:"Which emperor opted to retire and become a cabbage farmer?",
 		choices:["Constantine", "Diocletian", "Domitian", "Augustus"],
 		answer:"Diocletian",
 		funFact: "When civil war threatened and his supporters asked him to come back, he treated them to a long lecture on cabbages.",
+		img: "assets/images/Diocletian.jpg",
 	},
 	{
 		question:"Which Roman Emperor was known to have parties to look at ruptured hernias?",
 		choices:["Nero", "Claudius", "Justinian", "Elagabalus"],
 		answer:"Elagabalus",
 		funFact:"Some people speculate that Elagabalus was transgender because he occasionally referred to himself as a 'lady' and is rumored to have asked for reassignment surgery.",
+		img: "assets/images/elagabalus.jpg",
 	},
 	{
 		question:"Which emperor had his soldiers collect seashells and declared that he had conquered the sea?",
 		choices:["Titus", "Vespasian", "Caligula", "Tiberius"],
 		answer:"Caligula",
 		funFact: "He then proceeded to hold a triumph in Rome, where the soldiers displayed their 'treasure' of seashells.",
+		img: "assets/images/caligula.jpg",
 	},
 	{
 		question:"As a boy, which future emperor ordered his servant thrown in a fire because his bath was too hot?",
 		choices:["Commodus", "Severus Alexander", "Galba", "Domitian"],
 		answer:"Commodus",
 		funFact: "The other servants had to throw a sheep skin into the fire and tell him it was the servant to appease him.",
+		img: "assets/images/commodus.jpg",
 	},
 	{
-		question:"question",
-		choices:["a", "b", "c", "d"],
-		answer:"b",
-		funFact: "fact",
+		question:"Whose last words were 'Oh dear, I think I am becoming a god?",
+		choices:["Otho", "Vespasian", "Trajan", "Titus"],
+		answer:"Vespasian",
+		funFact: "Vespasian was known for being brutal but fair. His son, Domitian, was just known for being brutal.",
+		img: "assets/images/vespasian.jpg",
 	},
 	{
-		question:"question",
-		choices:["a", "b", "c", "d"],
-		answer:"c",
-		funFact:"fact",
+		question:"Which emperor was known to use emetics in order to eat more?",
+		choices:["Severus Alexander", "Lucius Verus", "Vitellius", "Pertinax"],
+		answer:"Vitellius",
+		funFact:"He was known to have delicacies contstantly brought to him on the battlefield.",
+		img: "assets/images/vitellius.jpg",
 	}
 ];
 var correct = 0;
@@ -78,7 +88,8 @@ function decrease(){
 	if (timer <= 0){
 		clockStop();
 		$main.empty();
-		$main.append("<p>Oops! You ran out of time. It was " + questionsObj[currentQ].answer + "! </p>");
+		$main.append("<p id = 'displayAnswer'>Oops! You ran out of time. It was " + questionsObj[currentQ].answer + "! </p>");
+		$main.append("<img src='"+ questionsObj[currentQ].img  + "' alt = '"+ questionsObj[currentQ].answer  + "'/>")
 		$main.append("<p><strong>Fun Fact: </strong>" + questionsObj[currentQ].funFact + "</p>");
 		currentQ ++;
 		gameOver();
@@ -105,7 +116,7 @@ function questionAppear(){
 		$(".choices").append("<button class='btn' id = 'choice-button' value= '" + questionsObj[currentQ].choices[i] + "'>" + questionsObj[currentQ].choices[i] + "</button>" + "<br>");
 		
 	};
-}
+};
 function start(){
 	event.preventDefault();
         $main.empty();
@@ -120,8 +131,8 @@ function checkGuess (){
 	if ($(this).val() === questionsObj[currentQ].answer && timer > 0){
 		clockStop();
 		$main.empty();
-		$main.append("<p>Correct! It was " + questionsObj[currentQ].answer + "! </p>");
-		// $main.append("<p> Fun Fact: </p>");
+		$main.append("<p id = 'displayAnswer'>Correct! It was " + questionsObj[currentQ].answer + "! </p>");
+		$main.append("<img src='"+ questionsObj[currentQ].img  + "' alt = '"+ questionsObj[currentQ].answer  + "'/>")
 		$main.append("<p><strong>Fun Fact: </strong>" + questionsObj[currentQ].funFact + "</p>");
 		currentQ ++;
 		correct ++;
@@ -131,26 +142,28 @@ function checkGuess (){
 	else{
 		clockStop();
 		$main.empty();
-		$main.append("<p>Sorry! It was " + questionsObj[currentQ].answer + "! </p>");
-		// $main.append("<p> Fun Fact: </p>");
+		$main.append("<p id = 'displayAnswer'>Sorry! It was " + questionsObj[currentQ].answer + "! </p>");
+		$main.append("<img src='"+ questionsObj[currentQ].img  + "' alt = '"+ questionsObj[currentQ].answer  + "'/>")
 		$main.append("<p><strong>Fun Fact: </strong>" + questionsObj[currentQ].funFact + "</p>");
 		currentQ ++;
 		gameOver();
-	}
+	};
 };
 
 // End the Game and restart
 function gameOver(){
 	
-	if(currentQ <= 9){
+	if(currentQ < 9){
 		setTimeout(questionAppear, 4000);;
 	} else {
+		setTimeout(function(){
 		$main.empty();
 		$main.append("<h1> Game Over! </h1>");
 		$main.append("<p> You got " + correct + "/10 correct </p>");
 		$main.append("<button class='btn btn-primary' id='start-button'>Restart</button>");
-	}
-}
+		}, 4000);
+	};
+};
 // Event Listeners for button clicks
 $(document).on("click", "#choice-button", checkGuess);
 $(document).on("click", "#start-button", start);
